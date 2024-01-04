f6117e343fe75f50ccc4520298a60ce50254d788
Danil <106477538+heaset@users.noreply.github.com>
2024-01-04 17:06:38 +0500

    Update windows-compile-vs.bat

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -242,11 +242,6 @@ call :get-extension-zip-from-github "arraydebug"            "%PHP_ARRAYDEBUG_VER
 call :get-extension-zip-from-github "encoding"              "%PHP_ENCODING_VER%"              "pmmp"     "ext-encoding"            || exit 1
 
 call :get-extension-zip-from-github "parallel" "%PHP_PARALLEL_VER%" "krakjoe" "parallel" || exit 1
-cd /D parallel
-nmake >>"%log_file%" 2>&1 || call :pm-fatal-error "Error compiling parallel extension"
-copy %MSBUILD_CONFIGURATION%\php_parallel.pdb "%DEPS_DIR%\bin\php_parallel.pdb" >>"%log_file%" 2>&1 || exit 1
-copy php_parallel.dll "%DEPS_DIR%\bin\php_parallel.dll" >>"%log_file%" 2>&1 || exit 1
-cd /D ..
 
 call :pm-echo " - crypto: downloading %PHP_CRYPTO_VER%..."
 git clone https://github.com/bukka/php-crypto.git crypto >>"%log_file%" 2>&1 || exit 1
